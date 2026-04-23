4774993a25d785ca9e6c008b2e9280a461698fc2
Mario Gutierrez <mario.ag188@gmail.com>
2026-04-23 16:22:40 -0500

    create prefabs for dummy pieces with animation and rotation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,21 +6,44 @@ public class Enemy : MonoBehaviour
     public Animator animator;
     public Health health;
 
+    [Header("Death FX")]
+    [SerializeField] private GameObject[] deathParts;
+    [SerializeField] private float spawnForce = 5;
+    [SerializeField] private float torque = 5;
+    [SerializeField] private float lifetime = 2;
+
     private void OnEnable()
     {
         health.OnDamaged += HandleDamage;
-       // health.OnDeath += HandleDeath;
+        health.OnDeath += HandleDeath;
     }
-
 
     private void OnDisable()
     {
         health.OnDamaged -= HandleDamage;
-       // health.OnDeath -= HandleDeath;
+        health.OnDeath -= HandleDeath;
     }
 
     private void HandleDamage()
     {
         animator.SetTrigger("isDamaged");
+    }
+
+    private void HandleDeath()
+    {
+        foreach (GameObject prefab in deathParts)
+        {
+            Quaternion rotation = Quaternion.Euler(0, 0, UnityEngine.Random.Range(0.5f, 1));
+            GameObject part = Instantiate(prefab, transform.position, rotation);
+
+            Rigidbody2D rb = part.GetComponent<Rigidbody2D>();
+
+            Vector2 randomDirection = new Vector2(UnityEngine.Random.Range(-1f, 1f), UnityEngine.Random.Range(0.5f, 1f)).normalized;
+            rb.linearVelocity = randomDirection * spawnForce;
+            rb.AddTorque(UnityEngine.Random.Range(-torque, torque), ForceMode2D.Impulse);
+            Destroy(part, lifetime);
+        }
+
+        Destroy(gameObject);
     }
 }
